20748386c028826a1b0222f90e987ff6c95ffe10
Veronika <akulova.veronika17@gmail.com>
2021-01-04 17:07:12 +0700

    changed values some orange buttons; began to set up buttons for operations

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-'use strict';
-
 window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+
     const allBtns = document.querySelectorAll('.calculator__button'),
         entryField = document.querySelector('.calculator__entry-field'),
-        ac = document.querySelector('button[value="AC"]');
+        ac = document.querySelector('button[value="AC"]'),
 
+        collectionOrangeBtns = document.getElementsByClassName('calculator__button-orange'),
+        arrFunc = [collectionOrangeBtns[0], collectionOrangeBtns[1], collectionOrangeBtns[2], collectionOrangeBtns[3]],
+        equality = document.querySelector('button[value="="]');
+
+    
     let string = '',
         permission = true, 
         counter = 1;
@@ -36,9 +41,9 @@ window.addEventListener('DOMContentLoaded', function() {
                 string = '';
                 entryField.textContent = '0';
             } else if(btn.value == '+/-') {
-                if(string.length == 0) {
-                    string += '-';
-                    entryField.textContent = string + entryField.textContent;
+                if(entryField.textContent == '0') {
+                    entryField.textContent == '-' + entryField.textContent;
+                    
                 } else if(string.length > 0 && string[0] != '-') {
                     string = '-' + string;
                     entryField.textContent = string;
@@ -56,6 +61,39 @@ window.addEventListener('DOMContentLoaded', function() {
         });
         
     }
+
+    let stringForCalculation = '',
+        arrForCalculation = [],
+        regexp = /[\/\*\-\+]/g,
+        mass = [],  // Массив из знаков
+        intermediateResult = 0, // Переменная, в кот будет записываться и перезаписываться промежуточный результат
+        total;  // итог всех расчётов
+
+
+    for(let item of arrFunc) {
+        item.addEventListener('click', function() {
+            stringForCalculation += (string + item.value);
+            string = '';
+            
+            console.log(stringForCalculation);
+            return stringForCalculation;
+        })
+    }
+
+    equality.addEventListener('click', function() {
+        // Создали массив из чисел м/у кот расположены знаки
+        arrForCalculation = stringForCalculation.split(regexp);
+
+        mass = stringForCalculation.match(regexp);
+
+        for(let i = 1; i < arrForCalculation.length; i++) {
+            culculation(arrForCalculation[i-1], arrForCalculation[i], i-1);
+        }
+        total = intermediateResult;
+        // console.log(total);
+        return total;
+
+    })
     function clearString(string) {
         string = (string.replace(/-/, '')).replace(/,/, '');
         if(string.length >= 9) {
@@ -63,6 +101,32 @@ window.addEventListener('DOMContentLoaded', function() {
             console.log('Достигнут максимум');
             return permission;
         }
+    }
+
+    function culculation(item1, item2, index) {
+        if(index == 0) {
+            if(mass[index] == '/') {
+                intermediateResult = (Number(item1)/Number(item2));
+            } else if (mass[index] == '-') {
+                intermediateResult = (Number(item1)-Number(item2));
+            } else if (mass[index] == '*') {
+                intermediateResult = (Number(item1)*Number(item2));
+            } else if (mass[index] == '+') {
+                intermediateResult = (Number(item1)+Number(item2));
+            }
+        } else {
+            if(mass[index] == '/') {
+                intermediateResult /= Number(item2);
+            } else if (mass[index] == '-') {
+                intermediateResult -= Number(item2);
+            } else if (mass[index] == '*') {
+                intermediateResult *= Number(item2);
+            } else if (mass[index] == '+') {
+                intermediateResult += Number(item2);
+            }
+        }
+        console.log(intermediateResult);
+        return intermediateResult;
     }
 
 });
