f882f99331f3473600a58ab41d285a35d28efb23
Veronika <akulova.veronika17@gmail.com>
2021-01-07 22:50:29 +0700

    rewrote js-code

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 window.addEventListener('DOMContentLoaded', function() {
     'use strict';
-
     const allBtns = document.querySelectorAll('.calculator__button'),
         entryField = document.querySelector('.calculator__entry-field'),
         ac = document.querySelector('button[value="AC"]'),
@@ -9,124 +8,183 @@ window.addEventListener('DOMContentLoaded', function() {
         arrFunc = [collectionOrangeBtns[0], collectionOrangeBtns[1], collectionOrangeBtns[2], collectionOrangeBtns[3]],
         equality = document.querySelector('button[value="="]');
 
-    
     let string = '',
-        permission = true, 
-        counter = 1;
+        total;  // итоговое значение, результат к-л операции
+
 
     for(let btn of allBtns) {
-        btn.addEventListener('click', function(e) {
-            if((!btn.value.search(/[0-9]/) || !btn.value.search(/,/i)) && permission) {
-
-                if(!btn.value.search(/[1-9]/)) {
-                    if(string.length == 1 && string[0] == '0') {
-                        string = '';
-                    } else if (string.length == 2 && string == '-0') {
-                        string = '-';
-                    }
+        btn.addEventListener('click', ()=> {
+            if(btn.value.match(/[0-9]/)) {
+                if(btn.value == '0' && string.length == 0) {
+                    console.log('0 не может стоять первым, если не задумано дробное число');
+                } else {
                     ac.textContent = 'C';
-                    entryField.textContent = (string += btn.value); 
-                } else if(btn.value == ',' && string.length > 0 && !/,/.test(string)) {
-                    entryField.textContent = (string += btn.value);
-                } else if(btn.value == '0') {
-                    if( (string.length == 1 && string[0] == '0') || (string.length == 2 && string == '-0') ) {
-                        console.log(string);
-                    }  else {
-                        entryField.textContent = (string += btn.value);
-                    }
+                    string += btn.value;
+                    fillEntryField(string);
+                }
+            
+            
+            // Настрока кнопки ','
+            } else if(btn.value == ',' && !string.includes(','))  {
+                if(string.length == 0) {
+                    string += '0,';
+                    ac.textContent = 'C';
+                    fillEntryField(string);
+                } else {
+                    string += ',';
+                    fillEntryField(string);
                 }
 
-            } else if(btn.value == 'AC') {
-                ac.textContent = 'AC';
-                string = '';
-                entryField.textContent = '0';
+            // Настрока кнопки '+/-'
             } else if(btn.value == '+/-') {
-                if(entryField.textContent == '0') {
-                    entryField.textContent == '-' + entryField.textContent;
-                    
-                } else if(string.length > 0 && string[0] != '-') {
-                    string = '-' + string;
-                    entryField.textContent = string;
+                if(string.length > 1 || (string.length == 1 && string[0] != '-')) {
+                    if(string.includes('-')) {
+                        string = string.replace('-', '');
+                    } else {
+                        string = '-' + string;
+                    }
+                    fillEntryField(string);
+                } else if(string.length == 0){
+                    string += '-';
+                    fillEntryField(string + 0);
+                } else if(string == '-') {
+                    string = string.replace('-', '');
+                    fillEntryField(string + 0);
                 }
-                // counter += 1;
-                // if(counter % 2 != 0) {
-                //     string = string.replace('-', '');
-
-                // } 
-                // console.log(counter);
+                
+            
+            // Настрока кнопки '%'
+            } else if(btn.value == '%' && string.length != 0) {
+                if(string == '-' || string == '0,' || string == '-0,') {
+                    string = '';
+                    fillEntryField(string + 0);
+                } else {
+                    total = calcPercent(string);
+                    fillEntryField(total);
+                    string = '';
+                }
             }
-            clearString(string);
-            return string;
-
         });
-        
     }
+
+    // Настройка кнопки 'AC'
+    ac.addEventListener('click', ()=> {
+        ac.textContent = 'AC';
+        clearVar();
+        fillEntryField(0 + string);
+    });
+
+    // Запись в поле калькулятора
+    function fillEntryField(text) {
+        entryField.textContent = text;
+    }
+
+    // Функция отчистки переменных при нажатии на AC
+    function clearVar() {
+        string = '';
+        stringForCalculation = '';
+        total = '';
+        arrTotals = [];
+    }
+    // Функция проверки последнего символа в строке
+    function checkLastChar(string) {
+        if(string[string.length-1] == ',') {
+            string = string.replace(',', '');
+        }
+        return string;
+    }
+
+    // Расчёт процента от ста (%)
+    function calcPercent(string) {
+        let num,
+            result;
+
+        string = checkLastChar(string);
+
+        if(string.includes(',')) {
+            num = parseFloat(string.replace(',', '.'))/100;
+        } else {
+            num = Number(string)/100;
+        }
+
+        result = String(num).replace('.', ',');
+        return result;  
+    }
+
 
     let stringForCalculation = '',
-        arrForCalculation = [],
-        regexp = /[\/\*\-\+]/g,
-        mass = [],  // Массив из знаков
-        intermediateResult = 0, // Переменная, в кот будет записываться и перезаписываться промежуточный результат
-        total;  // итог всех расчётов
+        arrNums = [],
+        arrOperations = [],
+        arrTotals = [],
+        regexp = /[\/\*\-\+]/g;
 
 
-    for(let item of arrFunc) {
-        item.addEventListener('click', function() {
-            stringForCalculation += (string + item.value);
-            string = '';
-            
+    for(let func of arrFunc) {
+        func.addEventListener('click', ()=> {
+            if(string.length >= 1 && string != '-' && string != '0,' && string != '-0,') {
+                string = checkLastChar(string);
+
+                stringForCalculation += string;
+                stringForCalculation += func.value;
+                string = '';
+            } else if(string.length == 0 && total.length >= 1) {
+                stringForCalculation += total;
+                stringForCalculation += func.value;
+            }
             console.log(stringForCalculation);
-            return stringForCalculation;
-        })
+        });
     }
 
-    equality.addEventListener('click', function() {
-        // Создали массив из чисел м/у кот расположены знаки
-        arrForCalculation = stringForCalculation.split(regexp);
+    equality.addEventListener('click', ()=> {
+        string = checkLastChar(string);
 
-        mass = stringForCalculation.match(regexp);
+        stringForCalculation += string;
+        arrNums = stringForCalculation.split(regexp);
+        arrOperations = stringForCalculation.match(regexp);
 
-        for(let i = 1; i < arrForCalculation.length; i++) {
-            culculation(arrForCalculation[i-1], arrForCalculation[i], i-1);
+        for(let i = 0; i < arrNums.length; i++) {
+            if(arrNums[i].includes(',')) {
+                arrNums[i] = arrNums[i].replace(',', '.');
+            }
+
+            if(i == 1) {
+                arrTotals.push(culculation(arrNums[i-1], arrNums[i], i-1));
+                console.log(arrTotals);
+            } else if(i >= 2) {
+                arrTotals.push(culculation(arrTotals[i-2], arrNums[i], i-1));
+                console.log(arrTotals);
+            }
+            
         }
-        total = intermediateResult;
-        // console.log(total);
-        return total;
-
-    })
-    function clearString(string) {
-        string = (string.replace(/-/, '')).replace(/,/, '');
-        if(string.length >= 9) {
-            permission = false;
-            console.log('Достигнут максимум');
-            return permission;
+        total = String(arrTotals[arrTotals.length-1]);
+        if(total.includes('.')) {
+            total = total.replace('.', ',');
         }
-    }
+        fillEntryField(total);
+        clearVar();
+    });
 
     function culculation(item1, item2, index) {
-        if(index == 0) {
-            if(mass[index] == '/') {
-                intermediateResult = (Number(item1)/Number(item2));
-            } else if (mass[index] == '-') {
-                intermediateResult = (Number(item1)-Number(item2));
-            } else if (mass[index] == '*') {
-                intermediateResult = (Number(item1)*Number(item2));
-            } else if (mass[index] == '+') {
-                intermediateResult = (Number(item1)+Number(item2));
-            }
-        } else {
-            if(mass[index] == '/') {
-                intermediateResult /= Number(item2);
-            } else if (mass[index] == '-') {
-                intermediateResult -= Number(item2);
-            } else if (mass[index] == '*') {
-                intermediateResult *= Number(item2);
-            } else if (mass[index] == '+') {
-                intermediateResult += Number(item2);
-            }
-        }
-        console.log(intermediateResult);
-        return intermediateResult;
-    }
+        let itemTotal;
 
+        if(arrOperations[index] == '/') {
+            itemTotal = (Number(item1)/Number(item2)).toFixed(3);
+        } else if (arrOperations[index] == '-') {
+            itemTotal = (Number(item1)-Number(item2)).toFixed(3);
+        } else if (arrOperations[index] == '*') {
+            itemTotal = (Number(item1)*Number(item2)).toFixed(3);
+        } else if (arrOperations[index] == '+') {
+            itemTotal = (Number(item1)+Number(item2)).toFixed(3);
+        }
+        return itemTotal;
+    }
 });
+
+// function clearString(string) {
+//     string = (string.replace(/-/, '')).replace(/,/, '');
+//     if(string.length >= 9) {
+//         permission = false;
+//         console.log('Достигнут максимум');
+//         return permission;
+//     }
+// }
